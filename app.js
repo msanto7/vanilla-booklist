f771fe9e245c75a804f2fae6012bd472df12f8b8
MIKE <mike.santoro42@gmail.com>
2018-12-30 20:47:37 -0500

    html shell finished (js classes mapped)

---
+++ app.js
@@ -0,0 +1,20 @@
+// Book Class: Represents a Book
+class Book {
+    constuctor(title, author, isbn) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+    }
+}
+
+
+
+// UI Class: Handle UI Tasks
+
+// Store Class: Handles Local Storage
+
+// Event: Display Books 
+
+// Event: Add a Book
+
+// Event: Remove a Book
