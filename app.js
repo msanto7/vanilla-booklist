343b39e2e45b2a322a8e0108acc97ba9ac050aa1
MIKE <mike.santoro42@gmail.com>
2018-12-30 22:27:19 -0500

    validation and alerts working

--- app.js
+++ app.js
@@ -55,6 +55,20 @@ class UI {
 
     }
 
+    static showAlert(message, className) {
+        const div = document.createElement('div');
+        div.className = `alert alert-${className}`;
+        div.appendChild(document.createTextNode(message));
+        const container = document.querySelector('.container');
+        const form = document.querySelector('#book-form');
+        container.insertBefore(div, form);
+
+        //Alert Time Out
+        setTimeout(() => document.querySelector('.alert').remove(), 2000);
+
+
+    }
+
     static clearFields() {
         document.querySelector('#title').value = '';
         document.querySelector('#author').value = '';
@@ -78,19 +92,33 @@ document.querySelector('#book-form').addEventListener('submit', (e) => {
     const author = document.querySelector('#author').value;
     const isbn = document.querySelector('#isbn').value;
 
-    //Instantiate Book
-    const book = new Book(title, author, isbn);
-
-    //Add Book to UI
-    UI.addBookToList(book);
-
-    // Clear fields
-    UI.clearFields();
+    //Validate
+    if(title === '' || author === '' || isbn === '') {
+        //alert('Please fill in all fields');
+        UI.showAlert('Please fill in all fields', 'danger');
+    } else {
+        //Instantiate Book
+        const book = new Book(title, author, isbn);
     
+        //Add Book to UI
+        UI.addBookToList(book);
+
+        //Show Success Message 
+        UI.showAlert('Book Added', 'success');
+    
+        // Clear fields
+        UI.clearFields();
+
+    }
+      
 });
 
 
 // Event: Remove a Book
 document.querySelector('#book-list').addEventListener('click', (e) => {
     UI.deleteBook(e.target);
+
+     //Show Success Message 
+     UI.showAlert('Book Removed', 'success');
+
 });
