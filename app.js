323178c49a948a11056db84b76956eb97a51148f
MIKE <mike.santoro42@gmail.com>
2018-12-30 21:42:11 -0500

    remove and add book functional (not persistant)

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // Book Class: Represents a Book
 class Book {
-    constuctor(title, author, isbn) {
+    constructor(title, author, isbn) {
         this.title = title;
         this.author = author;
         this.isbn = isbn;
@@ -47,14 +47,50 @@ class UI {
 
         list.appendChild(row);
     }
+
+    static deleteBook(el) {
+        if (el.classList.contains('delete')) {
+            el.parentElement.parentElement.remove();
+        }
+
+    }
+
+    static clearFields() {
+        document.querySelector('#title').value = '';
+        document.querySelector('#author').value = '';
+        document.querySelector('#isbn').value = '';
+    }
 }
 
 // Store Class: Handles Local Storage
 
 // Event: Display Books 
-
 document.addEventListener('DOMContentLoaded', UI.displayBooks);
 
 // Event: Add a Book
+document.querySelector('#book-form').addEventListener('submit', (e) => {
+
+    //Prevent actual submit
+    e.preventDefault();
+
+    //get form values
+    const title = document.querySelector('#title').value;
+    const author = document.querySelector('#author').value;
+    const isbn = document.querySelector('#isbn').value;
+
+    //Instantiate Book
+    const book = new Book(title, author, isbn);
+
+    //Add Book to UI
+    UI.addBookToList(book);
+
+    // Clear fields
+    UI.clearFields();
+    
+});
+
 
 // Event: Remove a Book
+document.querySelector('#book-list').addEventListener('click', (e) => {
+    UI.deleteBook(e.target);
+});
